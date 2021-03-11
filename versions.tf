a39327bef9ae0982e2ea19d13495dd164f61c27d
jtsaito <jahn.saito@gmail.com>
2021-03-11 11:00:15 +0100

    Update required tf version to 0.14

--- versions.tf
+++ versions.tf
@@ -1,9 +1,10 @@
 terraform {
+  required_version = ">= 0.14"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.31.0"
     }
   }
-  required_version = ">= 0.13"
 }
