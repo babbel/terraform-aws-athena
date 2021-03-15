fcbf280f57b921f880ec94e873e3fe48f8c84b71
jtsaito <jahn.saito@gmail.com>
2021-03-15 14:05:23 +0100

    Add outputs for resources

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v1.0.1
+
+- [Output resource arns](https://github.com/babbel/terraform-aws-athena/pull/2)
+
 ## v1.0.0
 
 - [Initial version](https://github.com/babbel/terraform-aws-athena/pull/1)
--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,21 @@ output "iam-policy-document-athena-workspace-use" {
 
   value = data.aws_iam_policy_document.iam-policy-document-athena-workspace-use.json
 }
+
+output "athena_workgroup" {
+  description = "Athena workgroup."
+
+  value = aws_athena_workgroup.this
+}
+
+output "bucket" {
+  description = "S3 bucket for storing Athena data."
+
+  value = aws_s3_bucket.athena-workspace
+}
+
+output "glue_catalog_database" {
+  description = "Glue catalog database."
+
+  value = aws_glue_catalog_database.this
+}
