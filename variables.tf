0ebefe095eae12583daddf6e2abf7b08fc93481a
André Wendt <awendt@babbel.com>
2021-07-02 11:48:22 +0200

    Change type constraint for var.resource_specific_tags
    Without this, we get:
    
        Invalid value for "default" parameter: the default value must have the same
        type as the map elements.

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "name" {
 variable "resource_specific_tags" {
   description = "Map of tags to assign to specific resources supporting tags. Merged with `tags`."
 
-  type = map(string)
+  type = map(map(string))
 
   default = {}
 }
