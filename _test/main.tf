d3add11ba9f056f32d549beddabda2f291d83208
Jan Sebastian Siwy <git@jansiwy.de>
2022-06-09 11:45:43 +0000

    Run validation with multiple Terraform versions
    It's always the currently latest patch version for a minor version.

---
+++ _test/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "local"
+}
+
+module "athena" {
+  source  = "./.."
+
+  name                    = "alb-logs-example-production"
+  workspace_bucket_prefix = "athena-workgroup"
+  
+  tags = {
+    app  = "some-service"
+    env  = "production"
+  }
+}
